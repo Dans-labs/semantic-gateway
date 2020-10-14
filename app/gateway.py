67f36170ccd4a6bb27f184af95db568000ccc8ed
4tikhonov <4tikhonov@gmail.com>
2020-10-14 13:58:41 +0300

    Gateway merged with GUI

--- app/gateway.py
+++ app/gateway.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Developed by Slava Tykhonov and Eko Indarto
+# Data Archiving and Networked Services (DANS-KNAW), Netherlands
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
+from fastapi.templating import Jinja2Templates
+from starlette.responses import FileResponse, RedirectResponse
+from starlette.staticfiles import StaticFiles
+from src.model import Vocabularies, WriteXML
 from fastapi.openapi.utils import get_openapi
 from fastapi.middleware.cors import CORSMiddleware
 import xml.etree.ElementTree as ET
@@ -10,7 +16,6 @@ import re
 import os
 import json
 import urllib3, io
-import os
 
 def custom_openapi():
     if app.openapi_schema:
@@ -50,6 +55,8 @@ tags_metadata = [
 app = FastAPI(
     openapi_tags=tags_metadata
 )
+templates = Jinja2Templates(directory='templates/')
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 app.add_middleware(
     CORSMiddleware,
@@ -59,8 +66,39 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.openapi = custom_openapi
-
+configfile = '/app/conf/gateway.xml'
+if 'config' in os.environ:
+    configfile = os.environ['config']
 http = urllib3.PoolManager()
+
+@app.get('/configuration/xml')
+def get_configuration_xml():
+    try:
+        content = open(configfile, 'r')
+        return Response(content=content.read(), media_type="application/xml")
+    except:
+        return "NOT FOUND"
+
+@app.get('/configuration/view')
+def get_configuration_html_view(request: Request):
+    vocabularies = Vocabularies(configfile)
+    return templates.TemplateResponse('configuration-view.html', context={'request': request, 'ontologies': vocabularies.get_ontologies()})
+
+@app.get('/configuration/edit')
+def get_configuration_html_view(request: Request):
+    vocabularies = Vocabularies(configfile)
+    return templates.TemplateResponse('configuration-edit.html', context={'request': request, 'ontologies': vocabularies.get_ontologies()})
+
+@app.post("/configuration/edit")
+async def modify_configuration_post(request: Request):
+    form_data = await request.form()
+    writeXML = WriteXML(form_data.items());
+    writeXML.save(configfile)
+    return RedirectResponse(url='/configuration/view', status_code=302)
+
+@app.get('/configuration/download')
+def download():
+    return FileResponse(configfile, media_type='application/octet-stream', filename='gateway-conf.xml')
 
 @app.get("/{vocab}/{term}/", tags=["namespace"])
 def namespace(vocab: str, term: str, request: Request):
@@ -69,10 +107,6 @@ def namespace(vocab: str, term: str, request: Request):
 
 # READ configuration
 def readconfig(iparams):
-   configfile = '/app/conf/gateway.xml'
-   if 'config' in os.environ:
-       configfile = os.environ['config']
-
    tree = ET.parse(configfile)
    root = tree.getroot()
    vocabs = {}
@@ -167,6 +201,45 @@ def search(request: Request):
         data = cessda(config)
         return data
     return config
+
+@app.get('/dv/setting/edit')
+def get_fields_composer(request: Request):
+    r = http.request('GET', "https://raw.githubusercontent.com/ekoi/speeltuin/master/resources/CMM_Custom_MetadataBlock.tsv")
+    d = r.data.decode('utf-8')
+    s = io.StringIO(d)
+    template='{"aci":"ACI","source-name":"CVM_SERVER_NAME", "source-url":"CVM_SERVER_URL","vocabs":["VOC"],"keys": ["KV","KT","KU"]}';
+    json_process = ''
+    json_element = ''
+    json_text = ''
+    dv_setting_json = []
+    dv_setting_el = {}
+    for line in s:
+        abc = line.split('\t')
+        if json_process == '' and abc[1].endswith('-cv'):
+            json_element = template.replace('CVM_SERVER_NAME', 'CVM_SERVER_NAME-' + abc[1])
+            json_element = json_element.replace('CVM_SERVER_URL', 'CVM_SERVER_URL-' + abc[1])
+            json_element = json_element.replace('ACI',abc[1])
+            json_element = json_element.replace('VOC', abc[2])
+            json_process='create'
+        elif json_process  == 'create':
+            if abc[1].endswith('-vocabulary'):
+                json_element = json_element.replace('KV', abc[1])
+            elif abc[1].endswith('-term'):
+                json_element = json_element.replace('KT', abc[1])
+            elif abc[1].endswith('-url'):
+                json_element = json_element.replace('KU', abc[1])
+                json_process="finish";
+                dv_setting_el = json.loads(json_element)
+                dv_setting_json.append(dv_setting_el)
+
+            else:
+                print('error')
+
+        if json_process == 'finish':
+            json_process = ''
+
+        vocabularies = Vocabularies(configfile)
+    return templates.TemplateResponse('dv-cvm-setting-generator.html', context={'request': request, 'dv_setting_json' : dv_setting_json, 'ontologies': vocabularies.get_ontologies()})
 
 def cessda(config):
     data = json.loads(requests.get(config['apiurl']).text)
@@ -286,7 +359,7 @@ def create_cmm_element(u, c, pl, lpl):
     o_obj['language'] = {'type': 'literal', 'value': 'en'}
     return o_obj
 
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="0.0.0.0", port=9266)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9266)
 #if __name__ == "__main__":
 #    uvicorn.run(app, host="0.0.0.0")
